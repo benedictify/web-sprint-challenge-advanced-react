8f19e43ae85327be48406826ee0e0fa3387fd6fb
Benedict Schurwanz <benedictify@gmail.com>
2021-10-29 13:44:34 -0500

    create skeleton useForm function

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,9 @@
+import React, { useState } from "react";
+
 // write your custom hook here to control your checkout form
+
+const useForm = (props) => {
+	return props;
+}
+
+export default useForm;
