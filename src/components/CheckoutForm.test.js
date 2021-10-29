5c00c6ed2f299dca1bf6d227ebbbbd3bb3d34ede
Benedict Schurwanz <benedictify@gmail.com>
2021-10-29 14:57:18 -0500

    write initial render test, notes for second test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -5,6 +5,34 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+	render(<CheckoutForm/>);
+});
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", () => {
+	render(<CheckoutForm/>);
+	
+	// WHEN all forms are filled with valid data:
+	// look for div or element with class="success-message"
+
+	// what does it mean when an input is valid?
+	
+	// input name="firstName"
+	// any letters a-z/i
+
+	// input name="lastName"
+
+
+	// input name="address"
+
+
+	// input name="city"
+
+
+	// input name="state"
+
+
+	// input name="zip"
+
+
+});
